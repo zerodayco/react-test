31d26861179cce0ca0406786b3a58c277e01380a
dvankevich <dvankevich@gmail.com>
2025-02-07 11:39:25 +0200

    add redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+
 import "modern-normalize";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
